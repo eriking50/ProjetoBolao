a1481323dfa73db43d167dee4c6444ae0e4e6e2d
eriking50 <eriking5@hotmail.com>
2021-11-04 13:07:12 -0300

    fixes

--- src/repositories/ICampeonatoRepository.ts
+++ src/repositories/ICampeonatoRepository.ts
@@ -1,7 +1,7 @@
 import { Campeonato } from "models/CampeonatoEntity";
 
 export interface ICampeonatoRespository {
-    save(usuario: Campeonato): Promise<Campeonato>;
+    save(campeonato: Campeonato): Promise<Campeonato>;
     findByidCampeonatoApiExterna(idCampeonatoApiExterna: number): Promise<Campeonato>;
     findById(id: number): Promise<Campeonato>;
 }
