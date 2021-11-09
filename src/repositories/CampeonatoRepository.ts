8c3d477b1ec355e8af13c9a68329d85b29a74728
eriking50 <eriking5@hotmail.com>
2021-11-09 13:41:52 -0300

    Adicionado erro caso o usuario não esteja cadastrado nesse campeonato

--- src/repositories/CampeonatoRepository.ts
+++ src/repositories/CampeonatoRepository.ts
@@ -4,8 +4,10 @@ import { ICampeonatoRespository } from "./ICampeonatoRepository";
 
 @EntityRepository(Campeonato)
 export class CampeonatoRepository extends Repository<Campeonato> implements ICampeonatoRespository{
-    findBySlug(slug: string): Promise<Campeonato> {
-        return this.findOne(slug);
+    findBySlug(slugFind: string): Promise<Campeonato> {
+        return this.findOne({
+            where: { slug: slugFind }
+        });
     }
 
     findById(id: number) {
